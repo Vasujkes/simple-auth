5583d231ac63354ee34ea3f4a1b0249cc51e1709
Vasujke <dev.vasujke@gmail.com>
2020-06-04 03:46:34 +0300

    try fix https

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://emphasoft-test-assignment.herokuapp.com/";
+axios.defaults.baseURL = "https://emphasoft-test-assignment.herokuapp.com/";
 axios.defaults.headers.common["Authorization"] = window.localStorage.token;
 
 window.axios = axios;
